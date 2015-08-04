4dfb3ff0192115ed60452840e434aff2b7348964
byronlun <2664181855@qq.com>
2015-08-04 11:24:56 +0800

    quiz by js

--- quizByJavaScript.js
+++ quizByJavaScript.js
@@ -26,157 +26,16 @@ window.addEventListener('load', function() {
 	var B = getCookie('password');
 	if (A && B) {
 		//直接进入menu界面
+		alert('欢迎' + A);
 		funMenu();
+		signup.onclick = funSignup;
+		login.onclick = funlogin;
+		LOGIN.onclick = funLog;
 	} else {
 		//进入注册页面
-		//signup
-		signup.onclick = function() {
-			username.value = null;
-			password.value = null;
-			loginDiv.className = 'item hide';
-			sure.className = 'item';
-			var div = document.getElementById('addWords');
-			if (div != null) {
-				dljm.removeChild(div);
-			}
-			SURE.onclick = function() {
-				var userName = username.value;
-				var passWord = password.value;
-
-				//将新注册的用户名和密码放到newUserd对象里面
-				var newUser = {user: userName, pass: passWord}; 
-			 	//usersJSON变量用来存取用户及密码
-			 	var usersJSON = {data:[]};
-			 	if (localStorage.getItem('users')) {
-			 		 var usersJSON = JSON.parse(localStorage.getItem('users'));
-			 		 var temp = 0;
-			 		for (var i = 0; i < usersJSON.data.length; i ++) {
-			 		 	if (userName === usersJSON.data[i].user) {
-			 		 		temp =1;
-			 		 		break;
-			 		 	}
-			 		}
-			 		if (temp === 1) {
-			 		 	var div = document.createElement('div');
-						div.id = 'addWords';
-						var p1 = document.createElement('p');
-						var p2 = document.createElement('p');
-						p1.appendChild(document.createTextNode('User already exists!'));
-						p2.appendChild(document.createTextNode('Please login!'));
-						div.appendChild(p1);
-						div.appendChild(p2);
-						dljm.insertBefore(div, loginDiv);
-						username.value = null;
-						password.value = null;
-						sure.className= 'item hide';
-			 		} else {
-						funSuccess(usersJSON, newUser);
-			 		 }
-			 	} else {
-					funSuccess(usersJSON, newUser);
-			 	}
-			};
-		};
-		//login
-		login.onclick = function() {
-			username.value = null;
-			password.value = null;
-			loginDiv.className = 'item';
-			sure.className = 'item hide';
-			var div = document.getElementById('addWords');
-			if (div != null) {
-				dljm.removeChild(div);
-			}
-		};
-		//LOGIN
-		LOGIN.onclick = function() {
-		//验证用户信息
-			var userName = username.value;
-			var passWord = password.value;
-			var usersJSON = localStorage.getItem('users');
-			if (usersJSON) {//localStorage 里面保存有数据，进一步检查用户是否存在
-				var pass = false;
-				usersJSON = JSON.parse(usersJSON);
-				for(var i = 0; i < usersJSON.data.length; i++) {
-					if (usersJSON.data[i].user === userName) {
-						pass = true;
-						break;
-					}
-				}
-				if(pass) {//用户存在，进一步判断密码输入是否正确
-					var temp = false;
-					for(var i = 0; i < usersJSON.data.length; i++) {
-						if (usersJSON.data[i].user === userName && usersJSON.data[i].pass === passWord) {
-							temp = true;
-							break;
-						}
-					}
-					if(temp) {//用户存在并且密码输入正确
-						//cookie
-						var n = getCookie('username');
-						var p = getCookie('password');
-						if (n == userName && p ==passWord) {
-							alert('Welcome again ' + userName + '!');
-						} else {
-							setCookie('username', userName, 100);
-							setCookie('password', passWord, 100);
-						}
-						username.value = null;
-						password.value = null;
-						//进入menu界面
-						funMenu();
-					} else {//用户存在，但是密码输入错误
-						username.value = null;
-						password.value = null;
-						var div = document.getElementById('addWords');
-						if (div != null) {
-							dljm.removeChild(div);
-						}
-						var div = document.createElement('div');
-						div.id = 'addWords';
-						var p1 = document.createElement('p');
-						var p2 = document.createElement('p');
-						p1.appendChild(document.createTextNode('Password error!'));
-						p2.appendChild(document.createTextNode('Please relogin!'));
-						div.appendChild(p1);
-						div.appendChild(p2);
-						dljm.insertBefore(div, loginDiv);
-					}
-				} else {//用户不存在，提示返回注册页面
-					username.value = null;
-					password.value = null;
-					var div = document.getElementById('addWords');
-					if (div != null) {
-						dljm.removeChild(div);
-					}
-					var div = document.createElement('div');
-					div.id = 'addWords';
-					var p1 = document.createElement('p');
-					var p2 = document.createElement('p');
-					p1.appendChild(document.createTextNode('User no exists!'));
-					p2.appendChild(document.createTextNode('Please sign up!'));
-					div.appendChild(p1);
-					div.appendChild(p2);
-					dljm.insertBefore(div, loginDiv);
-				}
-			} else {//localStorage 没有数据，提示进入注册界面
-				username.value = null;
-				password.value = null;
-				var div = document.getElementById('addWords');
-				if (div != null) {
-					dljm.removeChild(div);
-				}
-				var div = document.createElement('div');
-				div.id = 'addWords';
-				var p1 = document.createElement('p');
-				var p2 = document.createElement('p');
-				p1.appendChild(document.createTextNode('User no exists!'));
-				p2.appendChild(document.createTextNode('Please sign up!'));
-				div.appendChild(p1);
-				div.appendChild(p2);
-				dljm.insertBefore(div, loginDiv);
-			}
-		};
+		signup.onclick = funSignup;
+		login.onclick = funlogin;
+		LOGIN.onclick = funLog;
 		function funSuccess(usersJSON, newUser) {
 			usersJSON.data.push(newUser);
 			usersJSON = JSON.stringify(usersJSON);
@@ -230,8 +89,8 @@ window.addEventListener('load', function() {
 			logout.onclick = function() {
 				dljm.className = 'main';
 				Menu.className = 'main hide';
-				setCookie('username','', new Date(0));
-				setCookie('password','', new Date(0));
+				unsetCookie('username');
+				unsetCookie('password');
 			};
 		}
 		function funLogout() {
@@ -422,5 +281,151 @@ window.addEventListener('load', function() {
 			setCookie(name,'', new Date(0));
 		}
 	}
+	function funSignup() {
+			username.value = null;
+			password.value = null;
+			loginDiv.className = 'item hide';
+			sure.className = 'item';
+			var div = document.getElementById('addWords');
+			if (div != null) {
+				dljm.removeChild(div);
+			}
+			SURE.onclick = function() {
+				var userName = username.value;
+				var passWord = password.value;
+
+				//将新注册的用户名和密码放到newUserd对象里面
+				var newUser = {user: userName, pass: passWord}; 
+			 	//usersJSON变量用来存取用户及密码
+			 	var usersJSON = {data:[]};
+			 	if (localStorage.getItem('users')) {
+			 		 var usersJSON = JSON.parse(localStorage.getItem('users'));
+			 		 var temp = 0;
+			 		for (var i = 0; i < usersJSON.data.length; i ++) {
+			 		 	if (userName === usersJSON.data[i].user) {
+			 		 		temp =1;
+			 		 		break;
+			 		 	}
+			 		}
+			 		if (temp === 1) {
+			 		 	var div = document.createElement('div');
+						div.id = 'addWords';
+						var p1 = document.createElement('p');
+						var p2 = document.createElement('p');
+						p1.appendChild(document.createTextNode('User already exists!'));
+						p2.appendChild(document.createTextNode('Please login!'));
+						div.appendChild(p1);
+						div.appendChild(p2);
+						dljm.insertBefore(div, loginDiv);
+						username.value = null;
+						password.value = null;
+						sure.className= 'item hide';
+			 		} else {
+						funSuccess(usersJSON, newUser);
+			 		 }
+			 	} else {
+					funSuccess(usersJSON, newUser);
+			 	}
+			};
+		}
+	function  funlogin() {
+			username.value = null;
+			password.value = null;
+			loginDiv.className = 'item';
+			sure.className = 'item hide';
+			var div = document.getElementById('addWords');
+			if (div != null) {
+				dljm.removeChild(div);
+			}
+		}
+	function funLog() {
+			//验证用户信息
+			var userName = username.value;
+			var passWord = password.value;
+			var usersJSON = localStorage.getItem('users');
+			if (usersJSON) {//localStorage 里面保存有数据，进一步检查用户是否存在
+				var pass = false;
+				usersJSON = JSON.parse(usersJSON);
+				for(var i = 0; i < usersJSON.data.length; i++) {
+					if (usersJSON.data[i].user === userName) {
+						pass = true;
+						break;
+					}
+				}
+				if(pass) {//用户存在，进一步判断密码输入是否正确
+					var temp = false;
+					for(var i = 0; i < usersJSON.data.length; i++) {
+						if (usersJSON.data[i].user === userName && usersJSON.data[i].pass === passWord) {
+							temp = true;
+							break;
+						}
+					}
+					if(temp) {//用户存在并且密码输入正确
+						//cookie
+						var n = getCookie('username');
+						var p = getCookie('password');
+						if (n == userName && p ==passWord) {
+							alert('Welcome again ' + userName + '!');
+						} else {
+							setCookie('username', userName, 100);
+							setCookie('password', passWord, 100);
+						}
+						username.value = null;
+						password.value = null;
+						//进入menu界面
+						funMenu();
+					} else {//用户存在，但是密码输入错误
+						username.value = null;
+						password.value = null;
+						var div = document.getElementById('addWords');
+						if (div != null) {
+							dljm.removeChild(div);
+						}
+						var div = document.createElement('div');
+						div.id = 'addWords';
+						var p1 = document.createElement('p');
+						var p2 = document.createElement('p');
+						p1.appendChild(document.createTextNode('Password error!'));
+						p2.appendChild(document.createTextNode('Please relogin!'));
+						div.appendChild(p1);
+						div.appendChild(p2);
+						dljm.insertBefore(div, loginDiv);
+					}
+				} else {//用户不存在，提示返回注册页面
+					username.value = null;
+					password.value = null;
+					var div = document.getElementById('addWords');
+					if (div != null) {
+						dljm.removeChild(div);
+					}
+					var div = document.createElement('div');
+					div.id = 'addWords';
+					var p1 = document.createElement('p');
+					var p2 = document.createElement('p');
+					p1.appendChild(document.createTextNode('User no exists!'));
+					p2.appendChild(document.createTextNode('Please sign up!'));
+					div.appendChild(p1);
+					div.appendChild(p2);
+					dljm.insertBefore(div, loginDiv);
+				}
+			} else {//localStorage 没有数据，提示进入注册界面
+				username.value = null;
+				password.value = null;
+				var div = document.getElementById('addWords');
+				if (div != null) {
+					dljm.removeChild(div);
+				}
+				var div = document.createElement('div');
+				div.id = 'addWords';
+				var p1 = document.createElement('p');
+				var p2 = document.createElement('p');
+				p1.appendChild(document.createTextNode('User no exists!'));
+				p2.appendChild(document.createTextNode('Please sign up!'));
+				div.appendChild(p1);
+				div.appendChild(p2);
+				dljm.insertBefore(div, loginDiv);
+			}
+		};
+
 }, false);
 //加一个计时器,还可以加一个计算题目完成和总题目的比例。
